f4d9262e7ed8bbd33793d61b491e8b90db0b6f15
rayyuan90 <rayyuan90@gmail.com>
2018-11-15 10:15:17 +0800

    add different run script

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,4 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
-module.exports = {
-  devtool: 'eval-source-map',
-  module: {
-    rules: [{
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      },
-      {
-        type: 'javascript/auto',
-        test: /\.json$/,
-        use: [{
-          loader: 'file-loader',
-          options: {}
-        }],
-        include: /\.\/config/
-      },
-      {
-        test: /\.html$/,
-        use: [{
-          loader: "html-loader"
-        }]
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      }
-    ]
-  },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: "./src/index.html",
-      filename: "./index.html"
-    })
-  ],
-  devServer: {
-    port: 8999,
-    historyApiFallback: true,
-    inline: true
-  }
-};
+module.exports =(env) => {
+ return require(`./webpack.${env}.js`)
+}
